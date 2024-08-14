582daae572a820f09699c3da3aaff73600efe5ac
Shubu <shubugyanwali@gmail.com>
2024-08-14 10:48:10 +0545

    added color grid

--- lib/logicpart/presentation/widgets/color_bottom_navvbar.dart
+++ lib/logicpart/presentation/widgets/color_bottom_navvbar.dart
@@ -2,27 +2,66 @@ import 'package:flutter/material.dart';
 
 class ColorBottomNavBar extends StatelessWidget {
   final List<Map<String, dynamic>> colorData;
-  final Function(List<String>) onColorSelected; // Expecting List<String>
+  final Function(List<String>) onColorSelected;
 
-  ColorBottomNavBar({required this.colorData, required this.onColorSelected});
+  const ColorBottomNavBar(
+      {super.key, required this.colorData, required this.onColorSelected});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      child: BottomNavigationBar(
-        items: colorData.map((data) {
-          return BottomNavigationBarItem(
-            icon: const Icon(Icons.color_lens, color: Colors.grey),
-            label: data['name'],
-          );
-        }).toList(),
-        onTap: (index) {
-          final List<String> colors =
-              List<String>.from(colorData[index]['colors']);
-          onColorSelected(colors); // Passing the list of colors
-        },
+    return BottomAppBar(
+      child: SizedBox(
+        height: 100,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: colorData.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                final List<String> colors =
+                    List<String>.from(colorData[index]['colors']);
+                onColorSelected(colors);
+              },
+              child: Container(
+                width: 100,
+                margin: const EdgeInsets.symmetric(horizontal: 1),
+                padding: const EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    mainAxisSpacing: 0,
+                    crossAxisSpacing: 0,
+                  ),
+                  itemCount: colorData[index]['colors'].length,
+                  itemBuilder: (context, colorIndex) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Color(
+                          _parseColor(colorData[index]['colors'][colorIndex]),
+                        ),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
+  }
+
+  int _parseColor(String color) {
+    color = color.toUpperCase().replaceAll("#", "");
+    if (color.length == 6) {
+      color = "FF$color";
+    }
+    return int.parse(color, radix: 16);
   }
 }
--- lib/logicpart/presentation/widgets/color_grid.dart
+++ lib/logicpart/presentation/widgets/color_grid.dart
@@ -1,40 +1,65 @@
 import 'package:flutter/material.dart';
 
-class ColorGrid extends StatelessWidget {
-  final List<String> colors;
+class ColorGrid extends StatefulWidget {
+  final List<String> selectedColors;
 
-  const ColorGrid({super.key, required this.colors});
+  const ColorGrid({super.key, required this.selectedColors});
+
+  @override
+  ColorGridState createState() => ColorGridState();
+}
+
+class ColorGridState extends State<ColorGrid> {
+  List<String> filledColors = [];
+
+  @override
+  void didUpdateWidget(ColorGrid oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.selectedColors.isNotEmpty) {
+      fillColorsOneByOne(widget.selectedColors);
+    }
+  }
+
+  void fillColorsOneByOne(List<String> newColors) {
+    for (var color in newColors) {
+      setState(() {
+        filledColors.add(color);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(8),
       child: ListView.builder(
-        scrollDirection: Axis.vertical, // Vertical scrolling for rows
-        itemCount: (colors.length / 5).ceil(), // Number of rows
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        itemCount: (filledColors.length / 5).ceil(),
         itemBuilder: (context, rowIndex) {
           final startIndex = rowIndex * 5;
-          final endIndex =
-              (startIndex + 5) > colors.length ? colors.length : startIndex + 5;
-          final rowColors = colors.sublist(startIndex, endIndex);
+          final endIndex = (startIndex + 5) > filledColors.length
+              ? filledColors.length
+              : startIndex + 5;
+          final rowColors = filledColors.sublist(startIndex, endIndex);
 
-          return Container(
-            height: 60, // Set the height for each row
-            child: SingleChildScrollView(
-              scrollDirection:
-                  Axis.horizontal, // Horizontal scrolling for each row
-              child: Row(
-                children: rowColors.map((color) {
-                  return Container(
-                    width: 50, // Width of each color container
-                    height: 50, // Height of each color container
-                    margin:
-                        EdgeInsets.all(4), // Margin around each color container
-                    color: Color(int.parse(color.substring(1), radix: 16) +
-                        0xFF000000), // Convert hex color string to Color
-                  );
-                }).toList(),
-              ),
+          return SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(5, (index) {
+                return Container(
+                  width: 50,
+                  height: 50,
+                  margin: const EdgeInsets.all(4),
+                  color: index < rowColors.length
+                      ? Color(
+                          int.parse(rowColors[index].substring(1), radix: 16) +
+                              0xFF000000)
+                      : Colors.white,
+                );
+              }),
             ),
           );
         },
